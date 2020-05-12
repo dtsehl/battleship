1cf39acb73dc0f89ea5b044afed25b869e119944
Cam C <ckc.cameron@gmail.com>
2020-05-11 20:37:52 -0600

    Writes name and lemgth methods for ship, failing test for health method

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -1,10 +1,27 @@
 require './lib/ship'
-require './minitest/autorun'
-require './minitest/pride'
+require 'minitest/autorun'
+require 'minitest/pride'
 
 class ShipTest < Minitest::Test
 
   def test_it_exists
+    cruiser = Ship.new("Cruiser", 3)
+
+    assert_instance_of Ship, cruiser
   end
+
+  def test_it_has_name_and_length
+    cruiser = Ship.new("Cruiser", 3)
+
+    assert_equal "Cruiser", cruiser.name
+    assert_equal 3, cruiser.length
+  end
+
+  def test_it_has_health
+    cruiser = Ship.new("Cruiser", 3)
+
+    assert_equal 3, cruiser.health
+  end
+
 
 end
