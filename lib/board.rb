da25e95037c6e0f5f0b982ccf587358003980d82
Dan Sehl <dan.sehl@gmail.com>
2020-05-12 19:55:56 -0600

    Refactor valid_placement and made progress on test passing

--- lib/board.rb
+++ lib/board.rb
@@ -18,14 +18,19 @@ class Board
   def valid_coordinate?(coordinate)
     self.cells.include?(coordinate)
   end
+  
 
   def valid_placement?(ship, coordinate)
-
-    coordinate.each_cons(ship.length).all? do |lat, long|
-    lat == long - 1
-  end
-
-    if coordinate.count == ship.length
+    coordinate.each_cons(ship.length).all? do |lat_ords, long_ords|
+      lat_ords = ["A".ord, "B".ord, "C".ord, "D".ord]
+      long_ords = ["1".ord, "2".ord, "3".ord, "4".ord]
+      if lat_ords.each_cons(ship.length).all? == true && long_ords.each_cons(ship.length).all? == true && coordinate.count == ship.length
+        true
+      else
+        false
+      end
+    end
+    if coordinate.count == ship.length && self.consecutive_coordinates == true
       true
     elsif coordinate.count != ship.length
       false
