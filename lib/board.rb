34c2d8f32f8810b6775559f765f064bb4f7ebc3e
Dan Sehl <dan.sehl@gmail.com>
2020-05-13 16:52:36 -0600

    Refactor valid placement? method

--- lib/board.rb
+++ lib/board.rb
@@ -26,20 +26,9 @@ class Board
 
   def valid_placement?(ship, coordinate)
     return false if ship.length != coordinate.length
-    lat_ords = coordinate.map {|coord| coord.split("").first}
-    long_ords = coordinate.map {|coord| coord.split("").last}
-    all_ords = coordinate.map {|coord| coord.split("")}
-
-    lat_ords_verification = lat_ords.each_cons(2).all? do |first, second|
-      first.ord + 1 == second.ord || first.ord == second.ord
-      end
-    long_ords_verification = long_ords.each_cons(2).all? do |first, second|
-      first.ord + 1 == second.ord || first.ord == second.ord
-      end
-    diag_verification = all_ords.each_cons(2).all? do |first, second|
+    ords = coordinate.map {|coord| coord.split("")}
+    verification = ords.each_cons(2).all? do |first, second|
       (first[0].ord + first[1].ord) + 1 == (second[0].ord + second[1].ord) || (first[0].ord + first[1].ord) == (second[0].ord + second[1].ord)
     end
-
-    lat_ords_verification && long_ords_verification && diag_verification
   end
 end
